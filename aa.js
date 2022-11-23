e7044103fefb0df2591981f200f4d30598baec38
SanjarDelevoper98 <s981114s@gmail.com>
2022-11-23 20:38:20 +0500

    Sanjar

---
+++ aa.js
@@ -0,0 +1,58 @@
+
+
+//          1 -- sonlar yi'indisini chiqarish
+
+
+
+// const number = parseInt(prompt('Istagan raqamingizni kiriting: '));
+
+// let sum = 0;
+
+// for (let i = 1; i <= number; i++) {
+//     sum += i;
+// }
+// console.log("Sonlar yig'indisi", sum);
+
+
+
+
+
+//           2 -- juft toq sonlar
+
+// let aa = +prompt("son yoz")
+// for(let i = aa; i<= aa; i++){
+//     i%2==0?console.log("juft"):console.log("toq");
+// }
+
+
+
+
+
+//           3-- yil oy kun
+
+// let yil = +prompt('Raqam kiriting')
+// alert(`${yil*24} soat ${yil*365} kun ${yil*52} hafta ${yil*12} oy bor`) 
+
+
+
+
+
+
+
+//           4-- async await
+
+
+// const url = async () => {
+//     let userurl = "https://jsonplaceholder.typicode.com/users"
+
+// let watch = await fetch(userurl);
+// let userjson = await watch.json();
+// console.log(userjson);
+
+// }
+
+// url()
+
+
+
+
